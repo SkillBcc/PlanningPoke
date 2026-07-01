0b4f559fed90ca07811b688c84f4efe8305e9c1f
Igor Pereira <skillbcc@gmail.com>
2026-07-01 15:07:07 +0100

    atualização para deploy no github

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,20 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Room from './pages/Room';
 import Terms from './pages/Terms';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/room/:roomId" element={<Room />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
